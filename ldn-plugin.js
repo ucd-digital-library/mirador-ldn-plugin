4609057125f8edfcd53ef1e2d8f64a2926d6b1a8
John Howard <john.brooks.howard@gmail.com>
2017-09-04 06:55:33 +0100

    Changes to event handlers, etc.
    Generalre-modelling of event handlers, other small changes

--- ldn-plugin.js
+++ ldn-plugin.js
@@ -1,7 +1,8 @@
 var Ldn = {
   /* options of the plugin */
-  options: {},
-
+  options: {
+  },
+  
   /* all of the needed locales */
   locales: {
     "de": {
@@ -23,43 +24,41 @@ var Ldn = {
       "supplementBtn": "Récupérer"
     }
   },
-
+  
   notificationButtonTemplate: Mirador.Handlebars.compile([
-    '<a href="javascript:;" role="button" title="{{t "notificationTooltip"}}" aria-label="{{t "notificationTooltip"}}" class="mirador-btn mirador-btn-inbox mirador-tooltip">',
-      '<i class="fa fa-bell fa-lg fa-fw"></i>',
-      '<span class="badge-container">',
-        '<span class="badge-count">{{count}}</span>',
-      '</span>',
-    '</a>',
-  ].join('')),
-
+  '<a href="javascript:;" role="button" title="{{t "notificationTooltip"}}" aria-label="{{t "notificationTooltip"}}" class="mirador-btn mirador-btn-inbox mirador-tooltip">',
+  '<i class="fa fa-bell fa-lg fa-fw"></i>',
+  '<span class="badge-container">',
+  '<span class="badge-count">{{count}}</span>',
+  '</span>',
+  '</a>',].join('')),
+  
   // notificationListTemplate: Mirador.Handlebars.compile([
   //   '<div class="notifications-wrapper">',
   //     '<h1>{{t "notifications"}}</h1>',
   //     '<ul class="notifications-wrapper-list"></ul>',
   //   '</div>'
   // ].join('')),
-
+  
   // notificationListItemTemplate: Mirador.Handlebars.compile([
   //   "<li><a href='javascript:;' class='supplement' data-url='{{notification}}' data-id='{{id}}'>{{notification}}</a></li>"
   // ].join('')),
-
+  
   notificationTemplate: Mirador.Handlebars.compile([
-    '<div class="notifications-wrapper">',
-      '<p>{{t "notificationModalMsg"}}</p>',
-      '<ul class="notifications-wrapper-list">',
-        '<li>',
-          '<div class="supplement-button">',
-            '<a href="javascript:;" class="supplement mirador-btn-supplement btn btn-success" data-url="{{notification}}" data-id="{{id}}">{{t "supplementBtn"}}</a>',
-          '</div>',
-          '<div class="supplement-info">',
-            '<p><a href="{{notification}}" rel="noopener" target="_blank">{{notification}}</a></p>',
-          '</div>',
-        '</li>',
-      '</ul>',
-    '</div>'
-  ].join('')),
-
+  '<div class="notifications-wrapper">',
+  '<p>{{t "notificationModalMsg"}}</p>',
+  '<ul class="notifications-wrapper-list">',
+  '<li>',
+  '<div class="supplement-button">',
+  '<a href="javascript:;" class="supplement mirador-btn-supplement btn btn-success" data-url="{{notification}}" data-id="{{id}}">{{t "supplementBtn"}}</a>',
+  '</div>',
+  '<div class="supplement-info">',
+  '<p><a href="{{notification}}" rel="noopener" target="_blank">{{notification}}</a></p>',
+  '</div>',
+  '</li>',
+  '</ul>',
+  '</div>'].join('')),
+  
   // notificationTemplate: Mirador.Handlebars.compile([
   //   '<table class="notifications-wrapper">',
   //     '<tr>',
@@ -67,41 +66,43 @@ var Ldn = {
   //     '</tr>',
   //   '</table>'
   // ].join('')),
-
+  
   /* initializes the plugin */
-  init: function(miradorInstance){
+  init: function (miradorInstance) {
     var _this = this;
-
-    i18next.on('initialized', function(){
+    i18next.on('initialized', function () {
       this.addLocalesToViewer();
-    }.bind(this));
-
-    miradorInstance.eventEmitter.subscribe("ADD_WINDOW", function(event, data){
-      _this.checkForNotifications(data);
-    });
-    // miradorInstance.eventEmitter.subscribe("focusUpdated", function(event, data){
-    //   console.log("focus", data);
-    //   _this.checkForNotifications(data);
-    // });
-
-    this.addEventHandlers();
+    }.bind(this));    
+    this.injectUserEventHandlers();
+    this.injectWindowEventHandler();
   },
-
+  
+  /* injects window event handler */
+  injectWindowEventHandler: function () {
+    var this_ = this;
+    var origFunc = Mirador.Window.prototype.bindEvents;
+    Mirador.Window.prototype.bindEvents = function () {
+      origFunc.apply(this);
+      //console.log(this.manifest);
+      this_.checkForNotifications(this);
+    }
+  },
+  
   /* injects the notification button to the window menu */
-  injectButtonToMenu: function(count, slot){
+  injectButtonToMenu: function (count, slot) {
     console.log("buttonslot", slot);
-    $(slot).find(".window-manifest-navigation").prepend(this.notificationButtonTemplate(
-      {"count": count}
-    ));
+    $(slot).find(".window-manifest-navigation").prepend(this.notificationButtonTemplate({
+      "count": count
+    }));
   },
-
+  
   /* injects notification list to the dom */
   // injectNotificationListToDom: function(slot){
   //   //console.log($(slot))
   //   //$(slot).append(this.notificationListTemplate());
   //   //$(slot).append(this.notificationsDialogTemplate());
   // },
-
+  
   // injectNotificationListItemToDom: function(notification, slot, id){
   //   //$(slot).find("#notifications-wrapper-list").append(this.notificationListItemTemplate({
   //   //$(slot).find("#notifications-wrapper-list").append(this.notificationTemplate({
@@ -112,12 +113,12 @@ var Ldn = {
   //   });
   //   //}));
   // },
-
+  
   /* adds event handlers mirador */
-  addEventHandlers: function(){
+  injectUserEventHandlers: function () {
     var _this = this
-
-    $(document).on("click", ".mirador-btn-inbox", function(e){
+    
+    $(document).on("click", ".mirador-btn-inbox", function (e) {
       // if ($(e.target).hasClass("selected")){
       //   $(e.target).removeClass("selected")
       //   _this.removeNotifications(e);
@@ -131,7 +132,7 @@ var Ldn = {
       _this.showNotifications(e);
       // }
     }.bind(this));
-
+    
     // $(document).on("click", "#notifications", function(e){
     //   if ($(e.target).hasClass("selected")){
     //     $(e.target).removeClass("selected")
@@ -142,34 +143,33 @@ var Ldn = {
     //     _this.showNotifications(e);
     //   }
     // }.bind(this));
-
-    $(document).on("click", ".supplement", function(e){
+    
+    $(document).on("click", ".supplement", function (e) {
       var url = $(e.target).attr("data-url");
       var id = $(e.target).attr("data-id");
       _this.retrieveData(url, id);
       bootbox.hideAll();
     }.bind(this));
-
+    
     // $(document).on("click", "#notifications-close", function(e){
     //   _this.removeNotifications(e);
     // }.bind(this));
   },
-
+  
   /* adds the locales to the internationalization module of the viewer */
-  addLocalesToViewer: function(){
-    for(var language in this.locales){
+  addLocalesToViewer: function () {
+    for (var language in this.locales) {
       i18next.addResources(
-        language, 'translation',
-        this.locales[language]
-      );
+      language, 'translation',
+      this.locales[language]);
     }
   },
-
+  
   // removeNotifications: function(e){
   //   $(e.target).closest(".slot").find("#notifications-wrapper").css({"display": "none"});
   // },
-
-  showNotifications: function(e){
+  
+  showNotifications: function (e) {
     var _this = this;
     new Mirador.DialogBuilder().dialog({
       title: i18next.t("notificationModalTitle"),
@@ -182,71 +182,70 @@ var Ldn = {
     });
     //$(e.target).closest(".slot").find("#notifications-wrapper").css({"display": "block"});
   },
-
-  checkForNotifications: function(data){
-    this.notification_urls = [];
+  
+  checkForNotifications: function (data) {
+    console.log('checking for notifications');
+    this.notification_urls =[];
     this.data = data;
-    var _this = this;
-
-      var serviceProperty = _this.data.manifest.jsonLd.service;
-      var service = [];
-      if (serviceProperty === undefined){
-        service = [];
-      }
-      else if (serviceProperty.constructor === Array){
-        for (var i = 0; i < serviceProperty.length; i++){
-          if (serviceProperty[i].profile === "http://www.w3.org/ns/ldp#inbox") {
-            //returns the first service object with the correct context
-            service.push(serviceProperty[i]);
-          }
+    var _this = this;    
+    
+    var serviceProperty = _this.data.manifest.jsonLd.service;
+    var service =[];
+    if (serviceProperty === undefined) {
+      service =[];
+    } else if (serviceProperty.constructor === Array) {
+      for (var i = 0; i < serviceProperty.length; i++) {
+        if (serviceProperty[i].profile === "http://www.w3.org/ns/ldp#inbox") {
+          //returns the first service object with the correct context
+          service.push(serviceProperty[i]);
         }
       }
-      else if (_this.data.manifest.jsonLd.service.profile === "http://www.w3.org/ns/ldp#inbox"){
-        service.push(_this.data.manifest.jsonLd.service);
-      }
-      //else {
-        //no service object with the right context is found
-        //service = [];
-      //}
-      if (service.length > 0){
-        var service_url = service[0]["@id"];
-
-          var inboxRequest = jQuery.ajax({
-            url: service_url,
-            dataType: 'json',
-            async: true
-          });
-
-          inboxRequest.done(function(data){
-            // 0 index means its only going to get the first notification
-            for (i = 0; i < data.contains.length; i++){
-              //var note_url = data.contains[0].url;
-              _this.notification_urls.push(data.contains[i].url);
-            }
-            _this.injectButtonToMenu(data.contains.length, _this.data.appendTo);
-            //$(".window-manifest-navigation").append("<a href='#' id='notifications'><i class='material-icons'>add_alert</i>" + data.contains.length + "</a>");
-            _this.insertNotifications();
-          });
-
+    } else if (_this.data.manifest.jsonLd.service.profile === "http://www.w3.org/ns/ldp#inbox") {
+      service.push(_this.data.manifest.jsonLd.service);
+    }
+    //else {
+    //no service object with the right context is found
+    //service = [];
+    //}
+    _this.insertNotifications(); // JBH
+    if (service.length > 0) {
+      var service_url = service[0][ "@id"];
+      
+      var inboxRequest = jQuery.ajax({
+        url: service_url,
+        dataType: 'json',
+        async: true
+      });
+      
+      inboxRequest.done(function (data) {
+        // 0 index means its only going to get the first notification
+        for (i = 0; i < data.contains.length; i++) {
+          //var note_url = data.contains[0].url;
+          _this.notification_urls.push(data.contains[i].url);
         }
+        _this.injectButtonToMenu(data.contains.length, _this.data.appendTo);
+        //$(".window-manifest-navigation").append("<a href='#' id='notifications'><i class='material-icons'>add_alert</i>" + data.contains.length + "</a>");
+        _this.insertNotifications();
+      });
+    }
   },
-
-  insertNotifications: function(){
+  
+  insertNotifications: function () {
     var _this = this;
     //var response = confirm("There is an available table of contents for this codex published by the Scholastic Commentaries and Text Archive (http://scta.info). Would you like to retrieve this table of contents?");
     //if (response === true){
     //jQuery(document.body).append("<div id='notifications-wrapper'><h1>Notifications of Related Content</h1><ul></ul></div>");
-    var slot = _this.data.appendTo
+    var slot = _this.data.appendTo;
     console.log("slot", slot)
     //this.injectNotificationListToDom(slot);
-    for (i = 0; i < _this.notification_urls.length; i++){
+    for (i = 0; i < _this.notification_urls.length; i++) {
       note_url = _this.notification_urls[i];
       var notificationRequest = jQuery.ajax({
         url: note_url,
         dataType: 'json',
         async: true
       });
-      notificationRequest.done(function(data){
+      notificationRequest.done(function (data) {
         var range_url = data.object;
         //_this.injectNotificationListItemToDom(range_url, slot, _this.data.id);
         // _this.notificationTemplate({
@@ -262,24 +261,23 @@ var Ldn = {
       });
     }
   },
-
-  retrieveData: function(url, id){
+  
+  retrieveData: function (url, id) {
     var _this = this;
     var rangeRequest = jQuery.ajax({
       url: url,
       dataType: 'json',
       async: true
     });
-    rangeRequest.done(function(data){
+    rangeRequest.done(function (data) {
       // could get the slot id from the dom, use this id to find the manifest
       // in thie _this.data.state object for this slot
       _this.data.manifest.jsonLd.structures = data.ranges;
-
       _this.data.eventEmitter.publish('structuresUpdated.' + id);
     });
   },
 };
 
-$(document).ready(function(){
-  Ldn.init(myMiradorInstance);
+$(document).ready(function () {
+  Ldn.init(Mirador);
 });
